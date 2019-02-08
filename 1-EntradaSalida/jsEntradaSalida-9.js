c235d76dc640567f33fbd2af47af6b89078ff4f9
alumno <alumno>
2019-02-08 12:01:21 -0300

    ejercicio 9 terminado

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,17 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+	var Sueldo;
+	var Aumento;
+	var SueldoFinal;
 
+	Sueldo=sueldo.value;
+
+	Sueldo=parseInt(Sueldo);
+
+	Aumento=Sueldo*0.1;
+	SueldoFinal=Sueldo+Aumento;
+
+	resultado.value=SueldoFinal;
 	
 }
