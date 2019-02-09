6a74205018f3557d3f53df06cac0dc49a88441cf
ivanbarreira <you@example.com>
2019-02-09 12:34:43 -0300

    termine el 1 del tp pero no funciona bien

--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -5,13 +5,64 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
-	
+	var PrimerPrecio;
+	var SegundoPrecio;
+	var TercerPrecio;
+	var Suma;
+
+	PrimerPrecio=PrecioUno.value;
+	SegundoPrecio=PrecioDos.value;
+	TercerPrecio=PrecioTres.value;
+
+	PrimerPrecio=parseInt(PrimerPrecio);
+	SegundoPrecio=parseInt(SegundoPrecio);
+	TercerPrecio=parseInt(TercerPrecio);
+
+	Suma=PrimerPrecio+SegundoPrecio+TercerPrecio;
+
+	alert(Suma);
 }
 function Promedio () 
 {
-	
+	var PrimerPrecio;
+	var SegundoPrecio;
+	var TercerPrecio;
+	var Suma;
+	var Promedio;
+
+	PrimerPrecio=PrecioUno.value;
+	SegundoPrecio=PrecioDos.value;
+	TercerPrecio=PrecioTres.value;
+
+	PrimerPrecio=parseInt(PrimerPrecio);
+	SegundoPrecio=parseInt(SegundoPrecio);
+	TercerPrecio=parseInt(TercerPrecio);
+
+	Suma=PrimerPrecio+SegundoPrecio+TercerPrecio;
+	Promedio=Suma/3;
+
+	alert(Promedio);
 }
 function PrecioFinal () 
 {
-	
+	var PrimerPrecio;
+	var SegundoPrecio;
+	var TercerPrecio;
+	var Suma;
+	var IVA;
+	var PrecioFinal;
+
+	PrimerPrecio=PrecioUno.value;
+	SegundoPrecio=PrecioDos.value;
+	TercerPrecio=PrecioTres.value;
+
+	PrimerPrecio=parseInt(PrimerPrecio);
+	SegundoPrecio=parseInt(SegundoPrecio);
+	TercerPrecio=parseInt(TercerPrecio);
+
+	Suma=PrimerPrecio+SegundoPrecio+TercerPrecio;
+	IVA=21/100;
+	PrecioFinal=suma+IVA;
+
+	alert(Preciofinal);
 }
