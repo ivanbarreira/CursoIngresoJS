6877a3d74fab49bdb0cce43ee4ffbcb0289f1878
ivanbarreira <you@example.com>
2019-02-09 11:11:35 -0300

    termine el 7

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,21 +4,72 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	
+	var PrimerNumero;
+	var SegundoNumero;
+	var Suma;
+
+	PrimerNumero=numeroUno.value;
+	SegundoNumero=numeroDos.value;
+
+	PrimerNumero=parseInt(PrimerNumero);
+	SegundoNumero=parseInt(SegundoNumero);
+
+	Suma=PrimerNumero+SegundoNumero;
+
+	alert("la suma es "+Suma);
+
 }
 
 function restar()
 {
-	
+	var PrimerNumero;
+	var SegundoNumero;
+	var Resta;
+
+	PrimerNumero=numeroUno.value;
+	SegundoNumero=numeroDos.value;
+
+	PrimerNumero=parseInt(PrimerNumero);
+	SegundoNumero=parseInt(SegundoNumero);
+
+	Resta=PrimerNumero-SegundoNumero;
+
+	alert("la resta es "+Resta);
+
 }
 
 function multiplicar()
 { 
-	
+	var PrimerNumero;
+	var SegundoNumero;
+	var Multiplicacion;
+
+	PrimerNumero=numeroUno.value;
+	SegundoNumero=numeroDos.value;
+
+	PrimerNumero=parseInt(PrimerNumero);
+	SegundoNumero=parseInt(SegundoNumero);
+
+	Multiplicacion=PrimerNumero*SegundoNumero;
+
+	alert("el producto es "+Multiplicacion);
+
 }
 
 function dividir()
 {
-	
+	var PrimerNumero;
+	var SegundoNumero;
+	var Division;
+
+	PrimerNumero=numeroUno.value;
+	SegundoNumero=numeroDos.value;
+
+	PrimerNumero=parseInt(PrimerNumero);
+	SegundoNumero=parseInt(SegundoNumero);
+
+	Division=PrimerNumero/SegundoNumero;
+
+	alert("el cociente es "+Division);
 }
 
