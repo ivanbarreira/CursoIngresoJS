6a115abdf0c0265fbc87d2b0e86222fe2646bed6
ivanbarreira <ivanchooh@gmail.com>
2019-02-16 10:48:14 -0300

    termine el 10 de if

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -2,23 +2,26 @@ function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
 	var Nota;
-
-	Nota=Math.floor(Math.random() * 10) +1; 
 	var mensaje;
+
+	Nota=Math.floor(Math.random() * 10) +1;
+
 	if(Nota>8)
 	{
-		mensaje=" excelente";
-	}else
+		mensaje=" EXCELENTE";
+	}
+	else
 	{
 		if(Nota>3)
 		{
 			mensaje=" APROBO";
-		}else
+		}
+		else
 		{
 			mensaje=" Vamos, la proxima se puede";
 		}
 	}
 	
-	alert("su nota es :"+ Nota + mensaje);
+	alert("su nota es "+ Nota +":"+ mensaje);
 
 }//FIN DE LA FUNCIÓN
