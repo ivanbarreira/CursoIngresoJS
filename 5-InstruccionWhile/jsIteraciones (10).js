2b31f42272f7cce33c3dce13c103f9bc03ea3b62
ivanbarreira <ivanchooh@gmail.com>
2019-02-28 12:10:48 -0300

    termine el 10 de while

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -10,11 +10,20 @@ function mostrar()
 	var sumaPositivos;
 	var cantidadPositivos;
 	var cantidadCeros;
+	var cantidadPares;
+	var promedioPositivos;
+	var promedioNegativos;
+	var diferencia;
 
 	respuesta="si";
 	cantidadPositivos=0;
 	cantidadNegativos=0;
 	cantidadCeros=0;
+	cantidadPares=0;
+	sumaPositivos=0;
+	sumaNegativos=0;
+
+
 
 	while(respuesta!="no")
 	{
@@ -38,9 +47,27 @@ function mostrar()
 				cantidadCeros++;
 			}
 		}
+		if(numeroIngresado%2==0)
+		{
+			cantidadPares++;
+		}
+
 
 		respuesta=prompt("ingrese `no´ para salir","escriba aqui");	
 	}
+	
+	promedioPositivos=sumaPositivos/cantidadPositivos;
+	promedioNegativos=sumaNegativos/cantidadNegativos;
+	diferencia=sumaPositivos-sumaNegativos;
+
+
 	console.log("suma de los positivos " + sumaPositivos);
 	console.log("suma de los negativos " + sumaNegativos);
-}//FIN DE LA FUNCIÓN
+	console.log("cantidad de positivos " + cantidadPositivos);
+	console.log("cantidad de negativos " + cantidadNegativos);
+	console.log("cantidad de ceros " + cantidadCeros);
+	console.log("cantidad de numeros pares " + cantidadPares);
+	console.log("promedio de los positivos " + promedioPositivos);
+	console.log("promedio de los negativos " + promedioNegativos);
+	console.log("la diferencia entre los positivos y los negativos es " + diferencia);
+}//FIN DE LA FUNCIÓNNumero
