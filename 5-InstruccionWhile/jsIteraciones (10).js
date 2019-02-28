c6bcbb2922f45701c8d3d152622a130806df9545
ivanbarreira <ivanchooh@gmail.com>
2019-02-28 11:09:14 -0300

    hasta el 10 de while incompleto

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -1,18 +1,46 @@
 function mostrar()
 {
+/*
+-2 -5 3 8 0
+*/
+	var respuesta;
+	var numeroIngresado;
+	var sumaNegativos;
+	var cantidadNegativos;
+	var sumaPositivos;
+	var cantidadPositivos;
+	var cantidadCeros;
 
-	var contador=0;
-	//declarar contadores y variables 
-	
-	var respuesta="si";
+	respuesta="si";
+	cantidadPositivos=0;
+	cantidadNegativos=0;
+	cantidadCeros=0;
 
 	while(respuesta!="no")
 	{
-		
-	
+		numeroIngresado=prompt("ingrese un numero","escriba aqui");
+		numeroIngresado=parseInt(numeroIngresado);
+
+		if(numeroIngresado<0)
+		{
+			sumaNegativos=sumaNegativos+numeroIngresado;
+			cantidadNegativos++;
+		}
+		else
+		{
+			if(numeroIngresado>0)
+			{
+				sumaPositivos=sumaPositivos+numeroIngresado;
+				cantidadPositivos++;
+			}
+			else
+			{
+				cantidadCeros++;
+			}
+		}
+
+		respuesta=prompt("ingrese `no´ para salir","escriba aqui");	
 	}
-
-
-
-
+	console.log("suma de los positivos " + sumaPositivos);
+	console.log("suma de los negativos " + sumaNegativos);
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -1,18 +1,36 @@
 function mostrar()
 {
 
-	var contador=0;
-	// declarar variables
-	
-	var respuesta='si';
+	var contador;
+	var respuesta;
+	var numeroMax;
+	var numeroMin;
+	var numeroIngresado;
+
+	contador=0;
+	respuesta='si';
+	numeroMax=-9999;
+	numeroMin=9999;
 
 	while(respuesta!='no')
 	{
-		
-	
+		numeroIngresado=prompt("ingrese un numero","escriba aqui");
+		numeroIngresado=parseInt(numeroIngresado);
+
+		if(numeroIngresado>numeroMax)
+		{
+			numeroMax=numeroIngresado;
+		}
+		if(numeroIngresado<numeroMin)
+		{
+			numeroMin=numeroIngresado;
+		}
+
+
+
+		respuesta=prompt("ingrese `no´ para salir","escriba aqui");
 	}
 
-
-
-
+	document.getElementById('maximo').value=numeroMax;
+	document.getElementById('minimo').value=numeroMin;
 }//FIN DE LA FUNCIÓN
